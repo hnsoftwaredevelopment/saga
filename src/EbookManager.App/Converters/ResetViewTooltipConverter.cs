3fc43bf76313529b8e7105de91b1d08ad4625afc
Herbert Nijkamp <herbie68@users.noreply.github.com>
2026-07-30 11:06:31 +0200

    Polish reset view action

---
+++ src/EbookManager.App/Converters/ResetViewTooltipConverter.cs
@@ -0,0 +1,34 @@
+using System.Globalization;
+using System.Windows.Data;
+using EbookManager.App.Localization;
+using EbookManager.Presentation.ViewModels;
+
+namespace EbookManager.App.Converters;
+
+public sealed class ResetViewTooltipConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        var template = values.Length > 0 ? values[0]?.ToString() : null;
+        var viewName = values.Length > 1 && values[1] is LibraryView view
+            ? LocalizedStrings.Current[ViewResourceKey(view)]
+            : string.Empty;
+
+        return string.Format(
+            CultureInfo.CurrentCulture,
+            string.IsNullOrWhiteSpace(template) ? "{0}" : template,
+            viewName);
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+        targetTypes.Select(_ => Binding.DoNothing).ToArray();
+
+    private static string ViewResourceKey(LibraryView view) =>
+        view switch
+        {
+            LibraryView.Bookshelf => "BookshelfView",
+            LibraryView.Detailed => "DetailedView",
+            LibraryView.List => "ListView",
+            _ => "DetailedView"
+        };
+}
