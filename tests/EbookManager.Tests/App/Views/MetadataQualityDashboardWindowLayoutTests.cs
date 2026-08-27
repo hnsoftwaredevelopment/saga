197f649b70844df865da1d0c7c401dbacb3f147a
Herbie68 <herbert.nijkamp@gmail.com>
2026-08-27 15:43:17 +0200

    fix: make pane splitters visible and bounded

--- tests/EbookManager.Tests/App/Views/MetadataQualityDashboardWindowLayoutTests.cs
+++ tests/EbookManager.Tests/App/Views/MetadataQualityDashboardWindowLayoutTests.cs
@@ -20,11 +20,62 @@ public sealed class MetadataQualityDashboardWindowLayoutTests
             .Descendants(presentation + "GridSplitter")
             .Single(element => (string?)element.Attribute(xaml + "Name") == "IssuePaneSplitter");
 
-        issuePaneColumn.Attribute("Width")?.Value.Should().Be("320");
-        issuePaneColumn.Attribute("MinWidth")?.Value.Should().Be("240");
-        splitter.Attribute("Grid.Column")?.Value.Should().Be("1");
-        splitter.Attribute("ResizeDirection")?.Value.Should().Be("Columns");
-        splitter.Attribute("ResizeBehavior")?.Value.Should().Be("PreviousAndNext");
-        splitter.Attribute("Focusable")?.Value.Should().Be("True");
+        RequiredAttribute(issuePaneColumn, "Width").Should().Be("320");
+        RequiredAttribute(issuePaneColumn, "MinWidth").Should().Be("240");
+        RequiredAttribute(issuePaneColumn, "MaxWidth").Should().Be("600");
+        RequiredAttribute(splitter, "Grid.Column").Should().Be("1");
+        RequiredAttribute(splitter, "ResizeDirection").Should().Be("Columns");
+        RequiredAttribute(splitter, "ResizeBehavior").Should().Be("PreviousAndNext");
+        RequiredAttribute(splitter, "Focusable").Should().Be("True");
+        RequiredAttribute(splitter, "Style").Should().Be("{StaticResource VisibleVerticalGridSplitterStyle}");
     }
+
+    [Fact]
+    public void MainWindow_FilterPaneHasVisibleConstrainedSplitter()
+    {
+        var document = XDocument.Load(
+            Path.Combine(AppContext.BaseDirectory, "TestAssets", "MainWindow.xaml"));
+        XNamespace presentation = "http://schemas.microsoft.com/winfx/2006/xaml/presentation";
+        XNamespace xaml = "http://schemas.microsoft.com/winfx/2006/xaml";
+
+        var filterPaneColumn = document
+            .Descendants(presentation + "ColumnDefinition")
+            .Single(element => (string?)element.Attribute(xaml + "Name") == "LibraryFilterPaneColumn");
+        var splitter = document
+            .Descendants(presentation + "GridSplitter")
+            .Single(element => (string?)element.Attribute(xaml + "Name") == "LibraryFilterPaneSplitter");
+
+        RequiredAttribute(filterPaneColumn, "Width").Should().Be("220");
+        RequiredAttribute(filterPaneColumn, "MinWidth").Should().Be("180");
+        RequiredAttribute(filterPaneColumn, "MaxWidth").Should().Be("520");
+        RequiredAttribute(splitter, "Style").Should().Be("{StaticResource VisibleVerticalGridSplitterStyle}");
+        RequiredAttribute(splitter, "Focusable").Should().Be("True");
+    }
+
+    [Fact]
+    public void VerticalPaneSplitterStyle_HasVisibleGripAndInteractionFeedback()
+    {
+        var document = XDocument.Load(Path.Combine(AppContext.BaseDirectory, "TestAssets", "App.xaml"));
+        XNamespace presentation = "http://schemas.microsoft.com/winfx/2006/xaml/presentation";
+        XNamespace xaml = "http://schemas.microsoft.com/winfx/2006/xaml";
+
+        var style = document
+            .Descendants(presentation + "Style")
+            .Single(element => (string?)element.Attribute(xaml + "Key") == "VisibleVerticalGridSplitterStyle");
+        var grip = style
+            .Descendants(presentation + "Border")
+            .Single(element => (string?)element.Attribute(xaml + "Name") == "Grip");
+        var triggers = style.Descendants(presentation + "Trigger").ToArray();
+
+        style.Elements(presentation + "Setter")
+            .Should().Contain(element => (string?)element.Attribute("Property") == "Width" &&
+                                         (string?)element.Attribute("Value") == "12");
+        RequiredAttribute(grip, "Width").Should().Be("4");
+        triggers.Should().Contain(element => (string?)element.Attribute("Property") == "IsMouseOver");
+        triggers.Should().Contain(element => (string?)element.Attribute("Property") == "IsKeyboardFocused");
+    }
+
+    private static string RequiredAttribute(XElement element, XName name) =>
+        element.Attribute(name)?.Value ?? throw new InvalidOperationException(
+            $"Required attribute '{name}' is missing from '{element.Name.LocalName}'.");
 }
